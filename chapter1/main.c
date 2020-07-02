d932a4051de8152dbc157b13779e6b23d5e5fc8f
bjobek <bjornivar95@gmail.com>
2020-07-02 19:05:23 +0200

    Added Exercises up to 1.10

--- chapter1/main.c
+++ chapter1/main.c
@@ -29,8 +29,53 @@ return (t+b+n);
 //Copy input to output, replace each string of one or more blanks by a single blank
 void blanks2blank()
 {
+    int b,c;
+    b=c=0;
+
+    while((c=getchar()) != EOF){
+        if(c == ' ')
+        {
+            b++;
+        }
+        else
+        {
+            if(b>=2)
+            {
+                printf(" " );
+                putchar(c);
+                b=0;
+            }
+            putchar(c);
+            b=0;
+        }
+    }
 
 }
+
+void replace_t_b_bslash()
+{
+int c = 0;   
+ while((c=getchar()) != EOF){
+       
+        if(c == '\\')
+        {
+            printf("\\\\");
+        }
+        else if(c=='\t')
+        {
+
+            printf("\\t");
+        }
+        
+        else if(c=='\b')
+        {
+            printf("\\b");
+        }
+ } 
+}
+
+    
+
 
 
 
@@ -39,8 +84,10 @@ void blanks2blank()
 
 int main(void)
 {
-    unsigned int sum =  count_b_t_n();
-    assert(sum != 0);   
-    printf("In Total: %d\n",sum);
+   //unsigned int sum =  count_b_t_n();
+  //assert(sum != 0);   
+   //printf("In Total: %d\n",sum);
+   //blanks2blank();
+   replace_t_b_bslash();
     return 0;
 }
